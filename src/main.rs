1739dc8735843ce5151b2b037aca94f9409296f9
Martin Kustra <martin.kustra@aardwark.com>
2021-05-07 16:11:18 +0200

    Added simple mode selection

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,74 @@
-use std::time;
 use std::io::stdin;
-use std::num::ParseIntError;
+use std::time;
 
 mod binary_tree;
 mod vojsic_tree;
 
 fn main() {
+    print_menu();
+    let mode = read_input();
 
     println!("Input number of nodes:");
+    let node_count = read_input();
 
-    let node_count = match read_input() {
-        Ok(node_count) => node_count,
-        Err(why) => {
-            println!("Error parsing input: {}", why);
-            std::process::exit(1);
-        }
-    };
+    if mode == 1 {
+        time_comparison(node_count);
+        return;
+    }
 
-    println!("Unoptimized solution:");
-    time_solution(node_count, binary_tree::solution, vojsic_tree::solution);
+    if mode == 2 || mode == 4 {
+        println!("Unoptimized solution:");
+        time_solution(node_count, binary_tree::solution, vojsic_tree::solution);
+    }
 
-    println!("Optimized solution:");
-    time_solution(node_count, binary_tree::solution_optimized, vojsic_tree::solution_optimized);
+    if mode == 3 || mode == 4 {
+        println!("Optimized solution:");
+        time_solution(node_count, binary_tree::solution_optimized, vojsic_tree::solution_optimized);
+    }
 }
 
-fn read_input() -> Result<usize, ParseIntError> {
+fn print_menu() {
+    println!("Select mode:");
+    println!("1. Comparison");
+    println!("2. Unoptimized solution");
+    println!("3. Optimized solution");
+    println!("4. Both solutions");
+}
+
+fn read_input() -> usize {
     let mut buffer: String = String::new();
 
     stdin()
         .read_line(&mut buffer)
         .expect("Error reading stdin");
 
-    buffer.trim().parse()
+    match buffer.trim().parse() {
+        Ok(num) => num,
+        Err(why) => {
+            println!("Error parsing input: {}", why);
+            std::process::exit(1);
+        }
+    }
 }
 
 fn time_solution(node_count: usize, binary_solution: fn(usize) -> usize, vojsic_solution: fn(usize) -> usize) {
     let start_time = time::SystemTime::now();
 
     println!("Pocet uzlov: {}, pocet binarnych stromov: {}, pocet vojsicovych stromov: {}", node_count, binary_solution(node_count), vojsic_solution(node_count));
+
+    let end_time = time::SystemTime::now();
+    let duration = end_time.duration_since(start_time).expect("Time error");
+
+    println!("Solution took: {} ms\n", duration.as_millis());
+}
+
+fn time_comparison(node_count: usize) {
+    let start_time = time::SystemTime::now();
+
+    for i in 1..(node_count + 1) {
+        println!("Pocet uzlov: {}, pocet binarnych stromov: {}, pocet vojsicovych stromov: {}",
+                 node_count, binary_tree::solution_optimized(i), vojsic_tree::solution_optimized(i));
+    }
 
     let end_time = time::SystemTime::now();
     let duration = end_time.duration_since(start_time).expect("Time error");
